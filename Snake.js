9ca6925703c63b2de1b6cd9bf757ba39d3d41e1f
Tibor Vlasics <tibor.vlasics@gmail.com>
2021-02-27 11:38:13 +0100

    Add canvas drawing functions

--- Snake.js
+++ Snake.js
@@ -7,11 +7,32 @@ class Snake {
     this.context.scale(10, 10);
     this.arena = new Arena(40, 24);
     this.player = new Player();
+
+    this.colors = [
+      "#000",
+      "#ff0000",
+      "#00ff00",
+    ];
+
     this.draw();
   }
 
   draw() {
     this.context.fillStyle = '#000';
     this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
+
+    this._drawMatrix(this.arena.matrix, { x: 0, y: 0 });
+    this._drawMatrix(this.player.matrix, this.player.position);
+  }
+
+  _drawMatrix(matrix, offset) {
+    matrix.forEach((row, y) => {
+      row.forEach((value, x) => {
+        if (value !== 0) {
+          this.context.fillStyle = this.colors[value];
+          this.context.fillRect(x + offset.x, y + offset.y, 1, 1)
+        }
+      });
+    });
   }
 }
